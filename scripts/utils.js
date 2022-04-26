55f7338254173c07b7845030b049b31502278098
rubencito <ruben.santillan0902@gmail.com>
2022-04-26 14:17:32 -0500

    add utils to save and get data of local storage

---
+++ scripts/utils.js
@@ -0,0 +1,13 @@
+// import { appKey } from "./config.js";
+let appKey = "bsale_test";
+
+export function fromLocalStorage(key) {
+  const data = JSON.parse(localStorage.getItem(appKey)) || {};
+  return data[key];
+}
+
+export function saveToLocalStorage(key, value) {
+  let data = JSON.parse(localStorage.getItem(appKey)) || {};
+  data = { ...data, [key]: value };
+  localStorage.setItem(appKey, JSON.stringify(data));
+}
