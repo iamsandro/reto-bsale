e57f3e86994372c2db56572c124eef9d0d320356
rubencito <ruben.santillan0902@gmail.com>
2022-04-26 14:16:39 -0500

    create DOMHandler for add HTML code on index.html

---
+++ scripts/dom-handler.js
@@ -0,0 +1,15 @@
+const DOMHandler = {
+  module: null,
+  parent: null,
+  load(module, parent) {
+    this.module = module;
+    this.parent = parent;
+    parent.innerHTML = module; //toString()
+    module.addListeners();
+  },
+  reload() {
+    this.load(this.module, this.parent);
+  },
+};
+
+export default DOMHandler;
